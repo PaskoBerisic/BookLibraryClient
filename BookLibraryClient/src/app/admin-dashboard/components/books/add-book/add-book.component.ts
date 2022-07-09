17187473b5c9bfc706117cb2f53a59a0ae594add
Paško Berišić <Paki.berka@gmail.com>
2022-07-09 15:03:01 +0200

    Commit 09072022

--- BookLibraryClient/src/app/admin-dashboard/components/books/add-book/add-book.component.ts
+++ BookLibraryClient/src/app/admin-dashboard/components/books/add-book/add-book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,OnChanges, SimpleChanges} from '@angular/core';
+import { Component, OnInit,} from '@angular/core';
 import { Author } from 'src/app/models/author.model';
 import { Book } from 'src/app/models/book.model';
 import { Genre } from 'src/app/models/genre.model';
@@ -10,12 +10,13 @@ import { BookLibraryService } from 'src/app/services/book-library.service';
   templateUrl: './add-book.component.html',
   styleUrls: ['./add-book.component.css']
 })
-export class AddBookComponent implements OnInit, OnChanges {
+export class AddBookComponent implements OnInit {
   book: Book = {}
   languages: Language[] = [];
   genres: Genre[] = [];
   publishers: Publisher[] = [];
   authors: Author[] = [];
+  varijabla: any = {};
   constructor(private bookLibraryService: BookLibraryService) { }
 
   ngOnInit(): void {
@@ -40,23 +41,35 @@ export class AddBookComponent implements OnInit, OnChanges {
       console.log(this.authors);
     })
   }
-  ngOnChanges(changes: SimpleChanges): void{
-
-  }
+  
   addBook(book: Book){
-    this.bookLibraryService.postItem('Book', book);
-    console.log(book);
+    this.bookLibraryService.postItem('Book', this.varijabla);
+    console.log(this.varijabla);
   }
   addAuthor(id: number){
-    this.book.authors.id = id;
-  }
+    if(this.book.authors != undefined){
+      this.book.authors.id=1;  
+    }
+    }
+   
   addLanguage(id: number){
     this.book.languageId= id;
   }
   addGenre(id: number){
-    this.book.genres.id = id;
   }
   addPublisher(id: number){
     this.book.publisherId = id;
   }
-}
+  checkRequest(varijabla: any){
+    varijabla.title = 'stringic';
+    varijabla.authors = {id: 1};
+    varijabla.publisherId = 1;
+    varijabla.languageId = 1;
+    varijabla.description = 'tretertr'
+    varijabla.yearOfPublish= 1000;
+    varijabla.genres = {id: 1};
+    varijabla.rentalPrice = 1;
+    varijabla.unitNumber = 5;
+    console.log(varijabla);
+    }
+  }
